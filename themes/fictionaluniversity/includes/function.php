627deb01ca6ae4bb3bbcaade299b839b8acb1518
Olumaja <olumajasegun@gmai.com>
2021-02-19 09:12:38 +0100

    Dynamic banner

--- themes/fictionaluniversity/includes/function.php
+++ themes/fictionaluniversity/includes/function.php
@@ -36,3 +36,34 @@ function university_adjust_queries($query){
     }
 
 }
+
+//This function handle fallback for page banners
+function pageBanner($args = NULL){
+    if(!$args['title']){
+        $args['title'] = 'Temporal title';
+    }
+
+    if(!$args['subtitle']){
+        $args['subtitle'] = 'Temporal subtitle';
+    }
+
+    if(!$args['photo']){
+
+        $args['photo'] = get_theme_file_uri('/asset/images/ocean.jpg');
+        
+    }
+
+?>
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'] ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle'] ?></p>
+      </div>
+    </div>  
+</div>
+
+<?php 
+
+}
--- themes/fictionaluniversity/single-professor.php
+++ themes/fictionaluniversity/single-professor.php
@@ -3,18 +3,22 @@
 
     while(have_posts()){
         the_post();
+        if(get_field('page_banner_background_image')){
+            $photo = get_field('page_banner_background_image')['sizes']['pageBanner'];
+        }
+        else{
+          $photo = '';
+        }
+
+        pageBanner(
+            array(
+                'title' => get_the_title(),
+                'subtitle' => get_field('page_banner_subtitle'),
+                'photo' => $photo
+                
+              )
+        );
 ?>
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php $bannerImage = get_field('page_banner_background_image');
-        echo esc_url($bannerImage['sizes']['pageBanner']);
-    ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title() ?></h1>
-      <div class="page-banner__intro">
-        <p><?php the_field('page_banner_subtitle') ?></p>
-      </div>
-    </div>  
-</div>
 
 <div class="container container--narrow page-section">
     
