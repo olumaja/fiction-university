75a9cf66a493a8c54b56b157e86db789f5b37b34
Olumaja <olumajasegun@gmai.com>
2021-02-23 23:41:46 +0100

    ,

--- themes/fictionaluniversity/archive-event.php
+++ themes/fictionaluniversity/archive-event.php
@@ -1,14 +1,31 @@
-<?php get_header(); ?>
+<?php get_header(); 
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/asset/images/ocean.jpg') ?>);"></div>
+if(get_field('page_banner_background_image')){
+    $photo = get_field('page_banner_background_image')['sizes']['pageBanner'];
+}
+else{
+  $photo = '';
+}
+
+pageBanner(
+    array(
+        'title' => 'All Events',
+        'subtitle' => get_the_archive_description(),
+        'photo' => $photo
+        
+      )
+);
+?>
+
+<!-- <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php //echo get_theme_file_uri('/asset/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"> All Events </h1>
       <div class="page-banner__intro">
-        <p><?php the_archive_description() ?></p>
+        <p><?php //the_archive_description() ?></p>
       </div>
     </div>  
-</div>
+</div> -->
 
 <div class="container container--narrow page-section">
     <?php
--- themes/fictionaluniversity/archive.php
+++ themes/fictionaluniversity/archive.php
@@ -1,21 +1,11 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/asset/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"> <?php 
-        if(is_category()){
-          single_cat_title();
-        }
-        if(is_author()){
-            echo 'Post by '; the_author();
-        } 
-          ?> </h1>
-      <div class="page-banner__intro">
-        <p><?php the_archive_description() ?></p>
-      </div>
-    </div>  
-</div>
+    pageBanner(array(
+      'title' => get_the_archive_title(),
+      'subtitle' => get_the_archive_description(),
+      'photo' => ''
+    ))
+ ?>
 
 <div class="container container--narrow page-section">
     <?php
